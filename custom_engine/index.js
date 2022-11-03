a6c3bc77f9b8b33c6f47ef98e160077e309e18d1
HernanGH <hjcondori04@gmail.com>
2022-11-02 22:20:12 -0300

    CLASE 9  - motores de plantillas

--- custom_engine/index.js
+++ custom_engine/index.js
@@ -3,27 +3,30 @@ const express = require('express');
 
 const app = express();
 
-
 // defino el motor de plantilla
-app.engine('ntl', function (filePath, options, callback) {
+app.engine('coderhouse', function (filePath, options, callback) {
   fs.readFile(filePath, function (err, content) {
     if (err) {
       return callback(new Error(err));
     }
     const rendered = content.toString()
                             .replace('#title#', ''+ options.title +'')
+                            .replace('#subtitle#', ''+ options.subtitle +'')
                             .replace('#message#', ''+ options.message +'');
     return callback(null, rendered);
   });
 });
 
 app.set('views', './views'); // especifica el directorio de vistas
-app.set('view engine', 'ntl'); // registra el motor de plantillas
+app.set('view engine', 'coderhouse'); // registra el motor de plantillas
 
 // creamos el endpoint donde vamos a usar el motor de plantilla
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello world!'});
+  res.render('index', { 
+    title: 'Che', 
+    subtitle: 'Atencion...', 
+    message: 'se viene el mundial!!!'
+  });
 });
-
 
 app.listen(8080, () => console.log('Running in port: 8080'));
