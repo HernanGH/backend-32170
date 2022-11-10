9aa67ee5635475b5b93e7a660162fef3968cd18d
HernanGH <hjcondori04@gmail.com>
2022-11-09 22:38:07 -0300

    CHAT con socket

--- public/js/main.js
+++ public/js/main.js
@@ -2,10 +2,38 @@ const socket = io.connect();
 
 socket.on('message', (data) => {
   console.log(data);
-  alert(data);
   socket.emit('notification', 'Mensaje recibido exitosamente');
 });
+
+// socket.on('ALERTA ROJA', (data) => {
+//   console.log(data);
+  
+// })
 
 socket.on('messages', (data) => {
   console.log(data);
 })
+
+socket.on('chat-update', (data) => {
+  console.log('chat-update', data);
+  const chat = document.getElementById('chat');
+  const listItem = document.createElement('li');
+
+  listItem.innerHTML = data;
+  chat.appendChild(listItem);
+})
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const mensaje = document.getElementById('mensaje').value;
+
+  socket.emit('new-message', mensaje);
+})
+
+// const button = document.getElementById('button');}
+
+// button.addEventListener('click', (event) => {
+//   console.log(event);
+// })
--- server.js
+++ server.js
@@ -2,20 +2,33 @@ const express = require('express')
 const { Server: SocketServer } = require('socket.io')
 const { Server: HttpServer } = require('http')
 
+// 1 iniciamos app con express
 const app = express()
+// 2 creamos server http (nativo node) con la app express como parametro
 const httpServer = new HttpServer(app)
+// 3 creamos el socket server ahora si con el http server como parametro
 const io = new SocketServer(httpServer)
 
+// 4 creamos servidor web estico para poder ver los htmls (frontend)
 app.use(express.static('public'))
 
+// 5 creamos el handshake, (acuerdo con el cliente)
 io.on('connection', (socket) => {
-  console.log('Nuevo cliente conectado!')
+  console.log('Nuevo cliente conectado!', socket.id)
   
+  // mandamos 'message' a el cliente conectado
   socket.emit('message', 'Este es mi mensaje desde el servidor')
+  // socket.emit('ALERTA ROJA', { message: 'cuidado' });
 
   socket.on('notification', (data) => {
     console.log(data)
+    // mandamos 'messages' a todos los clientes conectados
     io.sockets.emit('messages', data)
+  })
+
+  socket.on('new-message', (newMessage) => {
+    console.log({newMessage});
+    io.sockets.emit('chat-update', newMessage)
   })
 })
 
