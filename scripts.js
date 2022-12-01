76f72b3e3852566c5fa97798d8e97a7cb64e6cdb
HernanGH <hjcondori04@gmail.com>
2022-11-30 22:29:19 -0300

    CLASE 17: MONGO

---
+++ scripts.js
@@ -0,0 +1,24 @@
+// mkdir base
+
+// mongod --dbpath ".../base"
+
+// mongo
+
+// show databases;
+
+// use sistema;
+
+// db.usuarios.insertOne({ nombre: 'Juan', edad: 32 });
+
+// db.usuarios.find();
+
+// db.usuarios.insertOne({ nombre: 'Pedro', edad: 30 });
+// db.usuarios.insertOne({ nombre: 'Julieta', edad: 25 });
+// db.usuarios.insertOne({ nombre: 'Cesar', edad: 45 });
+
+// db.usuarios.find();
+
+// db.productos.insertOne({ nombre: 'Pizza', precio: 30, stock: 10 });
+// db.productos.insertOne({ nombre: 'Banana', precio: 10, stock: 10 });
+// db.productos.insertOne({ nombre: 'Empanadas', precio: 25, stock: 10 });
+// db.productos.insertOne({ nombre: 'Ensalada', precio: 40, stock: 10 });
